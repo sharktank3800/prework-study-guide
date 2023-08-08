645f00258e5c47856b6c1f3d8b8af546723a05a9
[Ayman Shiham] <sharktank38000@gmail.com>
2023-08-08 17:06:25 -0400

    added basic javascript to the project

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+/*
+if (topic === 'HTML') {
+ console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+ console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+ console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+ console.log("Let's study JavaScript!");
+} else {
+ console.log('Please try again!');
+}
+*/
+
+
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for(var b = 0; b < topics.length; b++) {
+    console.log(topics[b]);
+}
+
